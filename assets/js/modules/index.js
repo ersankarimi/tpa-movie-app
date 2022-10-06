51fcd5c40c0b334b4cdd5b660453093eba9de921
Ersan Karimi <ersankarimii311@gmail.com>
2022-10-06 14:25:15 +0800

    Create index.js
    This file to import all modules and export it again

---
+++ assets/js/modules/index.js
@@ -0,0 +1,5 @@
+import { getMoviesData } from "./getMoviesData.js ";
+import { useState } from "./useState.js";
+import { createMovieElement } from "./createMovieElement.js";
+import { renderMovies } from "./renderMovies.js";
+export { getMoviesData, useState, createMovieElement, renderMovies };
