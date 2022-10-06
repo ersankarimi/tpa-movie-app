c763bd0344c12860584364a2073d80bd6113d370
Ersan Karimi <ersankarimii311@gmail.com>
2022-10-06 14:50:33 +0800

    Create index.js

---
+++ assets/js/modules/index.js
@@ -0,0 +1,6 @@
+import { getMoviesData } from "./getMoviesData.js ";
+import { useState } from "./useState.js";
+import { createMovieElement } from "./createMovieElement.js";
+import { renderMovies } from "./renderMovies.js";
+
+export { getMoviesData, useState, createMovieElement, renderMovies };
