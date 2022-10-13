86e0f0fc7c5135b4cf14755e2db7870cfa7a071d
ersankarimi <ersankarimii311@gmail.com>
2022-10-13 16:06:54 +0800

    Update

--- assets/js/modules/createMovieElement.js
+++ assets/js/modules/createMovieElement.js
@@ -1,28 +1,32 @@
-export const createMovieElement = (movieData) => {
-	const moviesWrapperElement = document.querySelector("#movies-wrapper");
-	// console.log(movieData);
-	const { original_title, vote_average, poster_path } = movieData;
+export const createMovieElement = ({
+  original_title,
+  vote_average,
+  poster_path,
+  release_date,
+}) => {
+  release_date = new Date(release_date).toDateString().split(" ").slice(1);
 
-	const cardWrapper = document.createElement("div");
-	cardWrapper.classList.add(
-		"card",
-		"col-12",
-		"col-sm-6",
-		"col-lg-4",
-		"col-xl-3",
-		"mb-4"
-	);
-	cardWrapper.style.border = "none";
-	cardWrapper.style.minHeight = "38rem";
-	cardWrapper.style.maxHeight = "38rem";
+  const moviesWrapperElement = document.querySelector("#movies-wrapper");
+  const cardWrapper = document.createElement("div");
+  cardWrapper.classList.add(
+    "card",
+    "col-12",
+    "col-sm-6",
+    "col-lg-4",
+    "col-xl-3",
+    "mb-4"
+  );
+  cardWrapper.style.border = "none";
+  cardWrapper.style.minHeight = "38rem";
+  cardWrapper.style.maxHeight = "38rem";
 
-	cardWrapper.innerHTML = `
+  cardWrapper.innerHTML = `
   <img
 					src="${
-						poster_path
-							? `https://www.themoviedb.org/t/p/w500${poster_path}`
-							: "https://us.123rf.com/450wm/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-comin.jpg?ver=6"
-					}"
+            poster_path
+              ? `https://www.themoviedb.org/t/p/w500${poster_path}`
+              : "https://us.123rf.com/450wm/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016/167492439-no-photo-or-blank-image-icon-loading-images-or-missing-image-mark-image-not-available-or-image-comin.jpg?ver=6"
+          }"
 					class="card-img-top h-75 img-fluid"
 					alt="${original_title} Poster"
 				/>
@@ -34,11 +38,13 @@ export const createMovieElement = (movieData) => {
 							${original_title}
 						</h5>
 						<h6 class="fw-semibold col-3 text-end">${parseFloat(vote_average).toFixed(
-							1
-						)}</h6>
+              1
+            )}</h6>
 					</div>
-					<p class="card-text">Apr 06, 2022</p>
+					<p class="card-text">${release_date[0]} ${release_date[1]}, ${
+    release_date[2]
+  }</p>
 				</div>`;
 
-	moviesWrapperElement.appendChild(cardWrapper);
+  moviesWrapperElement.appendChild(cardWrapper);
 };
--- assets/js/modules/renderMovies.js
+++ assets/js/modules/renderMovies.js
@@ -1,22 +1,22 @@
 import { createMovieElement } from "./createMovieElement.js";
 
 export const renderMovies = (moviesPromise) => {
-	const moviesWrapperElement = document.querySelector("#movies-wrapper");
-	moviesWrapperElement.innerHTML = "";
+  const moviesWrapperElement = document.querySelector("#movies-wrapper");
+  moviesWrapperElement.innerHTML = "";
 
-	moviesPromise.then((movie) => {
-		if (!movie.results.length) {
-			moviesWrapperElement.innerHTML = `
+  moviesPromise.then((movie) => {
+    if (!movie.results.length) {
+      moviesWrapperElement.innerHTML = `
 <div class="col-12 d-flex justify-content-center align-items-center">
   <h1 class="text-center">No movies found</h1>
 </div>
 `;
 
-			return;
-		}
+      return;
+    }
 
-		movie.results.forEach((movie) => {
-			return createMovieElement(movie);
-		});
-	});
+    movie.results.forEach((movie) => {
+      return createMovieElement(movie);
+    });
+  });
 };
